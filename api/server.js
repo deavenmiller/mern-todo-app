7fd13a34945c03da8f882fb70a910e2a92a92679
Deaven Miller <deavenmiller@gmail.com>
2023-11-15 21:39:43 -0800

    Add REST api to server.js & basic layout for App.js

--- api/server.js
+++ api/server.js
@@ -8,13 +8,49 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//mongoose.set('useNewUrlParser', true);
-//mongoose.set('useUnifiedTopology', true);
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
     .then(() => console.log("Connected to DB"))
     .catch(console.error);
+
+const Todo = require('./models/Todo');
+
+// Get the todos from db
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+
+    res.json(todos);
+})
+
+// Add tasks to todo list
+app.post('/todo/new', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save();
+
+    res.json(todo);
+})
+
+// Deletes tasks from todo list
+app.delete('/todo/delete/:id', async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id)
+
+    res.json(result);
+})
+
+// Updates the complete task status of a task
+app.put('/todo/complete/:id', async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+
+    todo.complete = !todo.complete;
+
+    todo.save();
+
+    res.json(todo);
+})
 
 app.listen(3001, () => console.log("Server started on port 3001"));
